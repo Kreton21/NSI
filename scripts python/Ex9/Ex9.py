79cb3185203c9c9ae0c2f7bfc182a74c6142894a
Kreton21 <kretonalex@gmail.com>
2021-09-14 08:59:39 +0200

    test

--- scripts python/Ex9/Ex9.py
+++ scripts python/Ex9/Ex9.py
@@ -1,7 +1,6 @@
 import sys
 import time as lethym
 import math
-
 def start():
     print("Formidable Fat Factor")
     print("Enter 1 for Height")
@@ -9,27 +8,27 @@ def start():
     ch=int(input())
     if ch == 1:
         ty=1
-        height()
+        height(ty)
     elif ch == 2:
         ty=2
         print("debug")
-        mass()
+        mass(ty)
     else:
         print("Veuillez entrer 1 ou 2")
         start()
 
-def height():
+def height(ty):
     print("Please enter height:")
     h=float(input())
     resl=18.5*h**2
     resh=25*h**2
-    compil(resl,resh)
-def mass():
+    compil(resl,resh,ty)
+def mass(ty):
     print("Please enter mass:")
     m=int(input())
     resl=m/18.5
     resh=m/25
-    compil(resh,resh)
+    compil(resh,resh,ty)
 def compil(resl,resh,ty):
     print("\r","Compiling please wait")
     an = ["[■□□□□□□□□□]/","[■■□□□□□□□□]\\", "[■■■□□□□□□□]/", "[■■■■□□□□□□]\\", "[■■■■■□□□□□]/", "[■■■■■■□□□□]\\", "[■■■■■■■□□□]/", "[■■■■■■■■□□]\\", "[■■■■■■■■■□]/", "[■■■■■■■■■■]\\"]
@@ -41,4 +40,6 @@ def compil(resl,resh,ty):
     if ty == 1:
         print("Vous devez peser entre",round(resl),"et",round(resh),"Kg")
     if ty == 2:
+        print("Vous devez etre entre",resl,"et",resh,"m")
+
 start()
