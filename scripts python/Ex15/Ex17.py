a1f801a7bcdd7af56fde736985729bd02b89ce2d
Kreton21 <kretonalex@gmail.com>
2021-09-21 08:31:01 +0200

    Ex 17 fini

--- scripts python/Ex15/Ex17.py
+++ scripts python/Ex15/Ex17.py
@@ -1,7 +1,10 @@
 import random as rand
-
 pari=int(input("Pari de de  \n"))
 for i in range(pari):
-    d=rand.randint(0,6)
+    d=rand.randint(1,6)
     print("De",i,"=",d)
-    # if d == 6
+    if d == 6:
+        print("Win apres",i+1,"essaies")
+        break
+    elif i>pari:
+        print("lose")
