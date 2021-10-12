8227cb59f2c634ed6f4f5d8b22e14def3ce41685
Kreton21 <kretonalex@gmail.com>
2021-10-12 10:08:58 +0200

    com

---
+++ scripts python/Tp2/Ex11.py
@@ -0,0 +1,6 @@
+def grog(n):
+    x=0
+    for i in range(n):
+        x+=1
+        print("b"+"r"*x+"hh")
+grog(10)
---
+++ scripts python/Tp2/Ex17.py
@@ -0,0 +1,4 @@
+def py(n):
+    for i in range (n):
+        print(""*round(i/2),"*"*i,""*i)
+py(5)
